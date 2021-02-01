c11aede09a3039aa940aeca26c3125230a1f0750
Cloud327 <roman.ermolaev@hotmail.com>
2021-02-01 14:50:02 +0100

    ny config fil

---
+++ configNew.py
@@ -0,0 +1,47 @@
+import json
+import os
+import lib2to3.fixes.fix_execfile
+import urllib.request
+import api1, api2, api3
+from flask import Flask, render_template, request
+from flask_restful import Resource, Api
+from flasgger import *
+from flask_cors import CORS, cross_origin
+# import json to load JSON data to a python dictionary
+import json
+
+# urllib.request to make a request to api
+import urllib.request
+
+class Testing:
+    def __init__(self):
+        pass
+
+    def test(self, apiArr):
+        a1 = apiArr[0]
+        a2 = apiArr[1]
+        a3 = apiArr[2]
+        app = Flask(__name__)
+
+        with app.app_context():
+            with app.test_request_context('http://127.0.0.1:8080/?city=dubai'):
+                a1.get(self)
+
+def main():
+    
+    
+    exec('api1')
+    exec('api2')
+    exec('api3')
+    
+    apiArr = [api1.API1, api2.API2, api3.API3]
+
+    t = Testing()
+
+    t.test(apiArr)
+
+
+
+
+if __name__ == "__main__":
+    main()
