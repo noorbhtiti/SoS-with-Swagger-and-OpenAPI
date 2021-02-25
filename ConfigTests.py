ce6691635389dce68e288bba4c8bf4d941df0d5d
Klas Mannberg <klas.mannberg@gmail.com>
2021-02-25 09:38:11 +0100

    auto pull interfaces

--- ConfigTests.py
+++ ConfigTests.py
@@ -2,20 +2,25 @@ import json
 import urllib.request
 from collections import defaultdict
 
-def TestTags(city,api_req,api_target,interface):
-	# TestTags(string,array,api_URL_req,api_URL_test,interface to test)
+def TestTags(api_req,api_target,context):
+	# TestTags(api_URL_req,api_URL_test,get function context)
 	#
 	# pulls the tags expected from api_req's target interface
 	# then compares data from api_target and check if resulting tags match 
-
-	if (interface==None):
-		interface='/'
 	
 	apisource = urllib.request.urlopen(api_req).read()
 	apidata = json.loads(apisource)
 	
+	print("aquiring interfacelist from api_req...")
+	if_source = apidata['paths'].keys()
+	interfaces = []
+	for x in if_source:
+		interfaces.append(x)
+	print("success! the source api interfacelist is: ")
+	print(interfaces)
+
 	print("aquiring taglist from api_req...")
-	tagssource = apidata['paths'][interface]['get']['responses']['200']['content']['application/json']['examples']['0']['value'] #can use this for more test cases later
+	tagssource = apidata['paths'][interfaces[0]]['get']['responses']['200']['content']['application/json']['examples']['0']['value'] #can use this for more test cases later
 	tags = []
 	for x in tagssource:
 		tags.append(x)
@@ -24,7 +29,7 @@ def TestTags(city,api_req,api_target,interface):
 	print(tags)
 	print("testing compability with api_target data results...")
 
-	source = urllib.request.urlopen(api_target+'?city=' + city).read()
+	source = urllib.request.urlopen(api_target+context).read()
 	data = json.loads(source)
 	for i in range(len(tags)):
 		print("testing tag " + tags[i]+"...")
@@ -40,7 +45,7 @@ def TestParams(api_req,api_target):
 	print(params)
 	if (params=='string'):
 		print('expected input is string, testing api_target')
-		source = urllib.request.urlopen(api_target+'?city=' + city).read()
+		source = urllib.request.urlopen(api_target+context).read()
 		data = json.loads(source)
 		print(data)
 
@@ -48,5 +53,5 @@ def TestParams(api_req,api_target):
 api1_doc = 'http://api.swaggerhub.com/apis/SoS_Temperature/API3/0.0.1'
 api3_url = 'http://127.0.0.1:8080/'
 
-TestTags('Oslo',api1_doc,api3_url,'/')
+TestTags(api1_doc,api3_url,'?city=Oslo')
 #TestParams(api1_doc,api3_url)
