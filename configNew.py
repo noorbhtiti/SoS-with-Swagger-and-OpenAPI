aae37e59ef1a9e8bbd90341bf1b314c0f8825c8c
noorbhtiti <43866697+noorbhtiti@users.noreply.github.com>
2021-03-06 14:07:53 +0100

    update

--- configNew.py
+++ configNew.py
@@ -1,17 +1,12 @@
 import json
-import os
-import lib2to3.fixes.fix_execfile
 import urllib.request
-import api1, api2, api3
-from flask import Flask, render_template, request
-from flask_restful import Resource, Api
-from flasgger import *
-from flask_cors import CORS, cross_origin
-# import json to load JSON data to a python dictionary
-import json
+import urllib.request
 
-# urllib.request to make a request to api
-import urllib.request
+from flask import *
+
+import api1
+import api2
+import api3
 
 
 class Testing:
@@ -51,20 +46,22 @@ class Testing:
         return taglist
 
 
-#def recursive(key, data, wantedTag):
-    #for key in data:
-    #    print(data[key])
-    #    print(len(data[key]))
-    #    if(len(data[key] != 0)
-            
+# def recursive(key, data, wantedTag):
+# for key in data:
+#    print(data[key])
+#    print(len(data[key]))
+#    if(len(data[key] != 0)
+
 
 taglist = [['in'], ['name'], ['type'], ['example'], ['temp']]
+
+
 def func1(data, wantedTag):
-    for key,value in data.items():
-        
-       # print (value)
-        
-        #print (str(key)+'->'+str(value))
+    for key, value in data.items():
+
+        # print (value)
+
+        # print (str(key)+'->'+str(value))
         if type(value) == type(dict()):
             return func1(value, wantedTag)
         elif type(value) == type(list()):
@@ -79,21 +76,21 @@ def func1(data, wantedTag):
 
 def func2(data, wantedTag):
     global taglist
-    for key,value in data.items():
-        if(str(key) == wantedTag):
+    for key, value in data.items():
+        if (str(key) == wantedTag):
             print("JAG ÄR HÄR")
             for i in range(len(taglist)):
-                if(taglist[i][0] == str(wantedTag)):
+                if (taglist[i][0] == str(wantedTag)):
                     taglist[i].append(str(value))
                     return
-        #print (str(key)+'->'+str(value))
+        # print (str(key)+'->'+str(value))
         if type(value) == type(dict()):
             func2(value, wantedTag)
-            
+
         elif type(value) == type(list()):
-            
+
             for val in value:
-                
+
                 if type(val) == type(str()):
                     pass
                 elif type(val) == type(list()):
@@ -101,26 +98,27 @@ def func2(data, wantedTag):
                 else:
                     func2(val, wantedTag)
 
+
 def func3(data, wantedTag):
-    for key,value in data.items():
-        if(str(key) == str(wantedTag)):
+    for key, value in data.items():
+        if str(key) == str(wantedTag):
             return str(value)
         if type(value) == type(dict()):
-            if(str(key) == str(wantedTag)):
+            if str(key) == str(wantedTag):
                 return func3(value, wantedTag)
             else:
                 func3(value, wantedTag)
-            
+
         elif type(value) == type(list()):
-            
+
             for val in value:
-                
-                if(str(key) == str(wantedTag)):
-                        
+
+                if (str(key) == str(wantedTag)):
+
                     return func3(val, wantedTag)
                 else:
-                        
-                    func3(val, wantedTag)    
+
+                    func3(val, wantedTag)
 
 
 def tagSearch2(apiDescAddress, taglist):
@@ -128,7 +126,7 @@ def tagSearch2(apiDescAddress, taglist):
     data = json.loads(source)
     for i in range(len(taglist)):
         print((func2(data, taglist[i][0])))
- 
+
 
 def main():
     exec('api1')
@@ -141,12 +139,13 @@ def main():
     api2DescAddress = 'https://api.swaggerhub.com/apis/SoS_Temperature/Api2/0.0.2'
 
     global taglist
-    #taglist = tagSearch2(api1DescAddress, taglist)
+    # taglist = tagSearch2(api1DescAddress, taglist)
     tagSearch2(api2DescAddress, taglist)
 
-    
     print(taglist)
-   # t = Testing(apiArr, api1DescAddress)
+
+
+# t = Testing(apiArr, api1DescAddress)
 
 # t = Testing(apiArr, api1DescAddress)
 
